9e5e019fef2c75e98b24f4a8f9dd3952a79fd358
MELANIEDELROCIOCONTRERASSANCHEZ <l21530262@cancun.tecnm.mx>
2024-11-12 23:29:53 -0500

    Add files via upload

---
+++ practica5.php
@@ -0,0 +1,29 @@
+<?php
+
+$dia = "Miercoles";
+
+switch ($dia){
+case "Lunes":
+	echo "Hoy es lunes";
+	break;
+case "Martes":
+	echo "Hoy es Martes";
+	break;
+case "Miercoles":
+	echo "Hoy es Miercoles";
+	break;
+case "Jueves":
+	echo "Hoy es Jueves";
+	break;
+case "Viernes":
+	echo "Hoy es Viernes";
+	break;
+case "Sabado":
+	echo "Hoy es Sabado";
+	break;
+Case "Domingo":
+	echo "Hoy es Domingo";
+	break;
+default:
+}
+?>
