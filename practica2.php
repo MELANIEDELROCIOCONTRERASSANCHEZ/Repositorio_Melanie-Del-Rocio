a5fdcc45d2464d9d92f3b1d54b5b294889023a4c
MELANIEDELROCIOCONTRERASSANCHEZ <l21530262@cancun.tecnm.mx>
2024-11-12 22:54:12 -0500

    Add files via upload

---
+++ practica2.php
@@ -0,0 +1,26 @@
+<?php
+
+$a=10; //Variable global
+
+function mensaje(){
+echo "<br>";
+echo "Hola Mundo";
+}
+
+mensaje(); //Mandamos a llamar a la funcion
+
+
+function variables(){
+global $a;
+$b=15; //variable local ya que no se declaro 
+echo "prueba de la impresion de variables dentro <br>";
+echo "la variable a es $a <br>";
+echo "la variable b es $b <br> <br>";
+}
+
+variables(); 
+echo "Prueba de la impresión de variables externas <br>";
+echo "la variable a es $a <br>";
+echo "la variable b es $b <br>";
+
+?>
