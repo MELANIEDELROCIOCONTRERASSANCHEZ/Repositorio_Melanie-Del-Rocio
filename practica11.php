0713596fd6cdaeead439f92446a114af9442b22c
MELANIEDELROCIOCONTRERASSANCHEZ <l21530262@cancun.tecnm.mx>
2024-12-03 18:21:08 -0600

    Add files via upload

---
+++ practica11.php
@@ -0,0 +1,32 @@
+<! DOCTYPE html>
+<html lang-"es"
+<head:
+<title> Formulario </title>
+<meta charset="UTF-8">
+<meta name="author" content="Alejandro Reyes">
+</head>
+<body>
+
+<h1>Registro de usuario</h1>
+<form action-"insertar.php" method-"POST">
+<label for -"nombre"> Nombre :< /label>
+<input type-"text" id-"nombre" name-"nombre" required> <br><br>
+
+<label for -"email"> Correo electronico :< /label>
+<input type-"email" id-"email" name-"email" required> <br><br>
+
+<label for -"edad"> Edad :< /label>
+<input type-"number" id-"edad" name-"edad" required> <br><br>
+
+<button type="submit">Enviar</button>
+</form>
+
+</body>
+</html>
+
+
+
+
+
+
+
